f2af72f2f8ce1274ee6c2d79b7ac73dd9da8c597
james-o <jaydhales@gmail.com>
2023-09-13 18:13:58 +0100

    Deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,7 +1,19 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  
+  const lmaoContract = await ethers.deployContract("LMAO");
+  await lmaoContract.waitForDeployment();
+  const wLmaoContract = await ethers.deployContract("WrappedLMAO", [
+    lmaoContract.target,
+  ]);
+
+  await wLmaoContract.waitForDeployment();
+
+  console.log({
+    "": "Deployed Contract",
+    LMAO: lmaoContract.target,
+    WLMAO: wLmaoContract.target,
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
