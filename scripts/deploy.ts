8f0d54581218c38dfcaec390e0f8671dc6ac58e0
james-o <jaydhales@gmail.com>
2023-09-13 18:30:25 +0100

    signer

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,8 @@
+import { AddressLike, Typed } from "ethers";
 import { ethers } from "hardhat";
 
 async function main() {
+  const [owners] = await ethers.getSigners();
   const lmaoContract = await ethers.deployContract("LMAO");
   await lmaoContract.waitForDeployment();
   const wLmaoContract = await ethers.deployContract("WrappedLMAO", [
@@ -14,6 +16,29 @@ async function main() {
     LMAO: lmaoContract.target,
     WLMAO: wLmaoContract.target,
   });
+
+  const getBalancesOF = async (addr: AddressLike | Typed, message: string) => {
+    const lmaoBalance = await lmaoContract.balanceOf(addr);
+    const wLmaoBalance = await wLmaoContract.balanceOf(addr);
+
+    console.log({
+      "": message,
+      lmaoBalance,
+      wLmaoBalance,
+    });
+  };
+
+  await getBalancesOF(owners, "")
+
+  await lmaoContract.approve(wLmaoContract, ethers.parseEther("1000000"));
+  await wLmaoContract.depositLMAO(ethers.parseEther("100"));
+
+  await getBalancesOF(owners, "")
+
+  await wLmaoContract.withdrawLMAO(ethers.parseEther("10"));
+
+  await getBalancesOF(owners, "")
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
